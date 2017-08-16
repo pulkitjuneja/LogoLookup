79604ed9cb25e46c77fabb3fbea28159a2867cc5
Pulkit Juneja <pulkit.16296@gmail.com>
2017-08-16 12:27:44 +0530

    vuforia SDK integrated

---
+++ Assets/Scripts/TrackerDetector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TrackerDetector : MonoBehaviour,ITrackableEventHandler {
+
+	private TrackableBehaviour mTrackableBehaviour;
+
+	void Start() {
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (mTrackableBehaviour)
+		{
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+	}
+
+	public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus,TrackableBehaviour.Status newStatus) {
+		if (newStatus == TrackableBehaviour.Status.DETECTED || newStatus == TrackableBehaviour.Status.TRACKED || newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) {
+			Debug.Log (mTrackableBehaviour.TrackableName);
+		}
+	}
+}
